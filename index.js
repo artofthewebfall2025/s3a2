29d75a580a8580a15e82315f61dcc588c70a60ca
Avery Townsend <codeshaunted@gmail.com>
2025-11-20 21:27:33 +0000

    Co-authored-by: ekeas <ekeas@users.noreply.github.com> Co-authored-by: Brian Lee <leebwj@users.noreply.github.com> Co-authored-by: ameliavc <ameliavc@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// taken from https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
+// // taken from https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
 
-function transformScroll(event) {
-    if (!event.deltaY) {
-        return;
-    }
+// function transformScroll(event) {
+//     if (!event.deltaY) {
+//         return;
+//     }
 
-    event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
-    event.preventDefault();
-}
+//     event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+//     event.preventDefault();
+// }
 
-var element = document.scrollingElement || document.documentElement;
-element.addEventListener('wheel', transformScroll);
+// var element = document.scrollingElement || document.documentElement;
+// element.addEventListener('wheel', transformScroll);
