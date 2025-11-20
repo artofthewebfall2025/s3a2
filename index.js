b9bff8d5efd3e8f39c2348263fcb55b389522df5
Avery Townsend <self@codeshaunted.com>
2025-11-20 16:07:04 -0500

    adsf

---
+++ index.js
@@ -0,0 +1,13 @@
+// taken from https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
+
+function transformScroll(event) {
+    if (!event.deltaY) {
+        return;
+    }
+
+    event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+    event.preventDefault();
+}
+
+var element = document.scrollingElement || document.documentElement;
+element.addEventListener('wheel', transformScroll);
