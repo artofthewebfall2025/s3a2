47b2c1dd727b6c463984f7ec8bf9e86efbb81b40
leebwj <109943383+leebwj@users.noreply.github.com>
2025-11-22 13:35:07 -0500

    Update (SVG + Toggle Animation)
    1. MockUp SVG Files Added
    2. MockUps set on the bottom of the page
    3. Don't know if this was intended but the hidden stockings was not showing when toggled. fixed to show stockings when toggled but pls feel free to change if that was not the intention.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const toggle = document.getElementById('xmasToggle');
 
 toggle.addEventListener('change', function () {
     if (this.checked) {
+        document.getElementById("sock").style.opacity = 0;
+        document.getElementById("stocking").style.opacity = 1;
         setTimeout(() => {
             window.location.href = 'https://ifttt.com/applets/d6EGULbv-last-minute-christmas-shopping-reminder';
         }, 400);
