eb592cf9cbb47e3dfe42ca34bb42b4edf9696caa
Avery Townsend <codeshaunted@gmail.com>
2025-11-22 18:07:06 +0000

    Co-authored-by: ekeas <ekeas@users.noreply.github.com> Co-authored-by: jagar-bhan <jagar-bhan@users.noreply.github.com> Co-authored-by: Brian Lee <leebwj@users.noreply.github.com> Co-authored-by: ameliavc <ameliavc@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// // taken from https://stackoverflow.com/questions/24639103/changing-vertical-scroll-to-horizontal
+const toggle = document.getElementById('xmasToggle');
 
-// function transformScroll(event) {
-//     if (!event.deltaY) {
-//         return;
-//     }
-
-//     event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
-//     event.preventDefault();
-// }
-
-// var element = document.scrollingElement || document.documentElement;
-// element.addEventListener('wheel', transformScroll);
+toggle.addEventListener('change', function () {
+    if (this.checked) {
+        setTimeout(() => {
+            window.location.href = 'https://ifttt.com/applets/d6EGULbv-last-minute-christmas-shopping-reminder';
+        }, 400);
+    }
+});
